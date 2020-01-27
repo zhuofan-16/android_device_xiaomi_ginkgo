be72b8d3e7af1026e8c22a220c68819379cac8b4
寒词ZhuoFan <4582h@dunearn.edu.sg>
2020-01-28 02:19:26 +0800

    Dot-ily

--- dot_ginkgo.mk
+++ dot_ginkgo.mk
@@ -6,7 +6,7 @@
 
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common dotOS stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
